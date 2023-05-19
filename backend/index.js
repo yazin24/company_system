1adb9b6d26fcad76d9367afb9f3a2194f750e400
yazin24 <yazinconiendo@gmail.com>
2023-05-19 14:22:31 +0800

    .

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on ${process.env.PORT}`);
+});
+
+mongoose.connect(process.env.DB_STRING)
+.then(() => {
+    console.log('Database Connection is Success!')
+}).catch((err) => {
+    console.error(err);
+})
+
