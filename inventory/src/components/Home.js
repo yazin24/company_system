f344d0b774bfae2e96572f18150c87909ef18616
yazin24 <yazinconiendo@gmail.com>
2023-05-23 13:35:53 +0800

    .

--- inventory/src/components/Home.js
+++ inventory/src/components/Home.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {FaPen, FaTrash} from 'react-icons/fa';
+import axios from 'axios';
 
 const Home = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const productData = async () => {
+            try {
+                const response = await axios.get('http://localhost:2400/product/list-products');
+                setData(response.data);
+            }catch(err) {
+                console.error(err);
+            }
+        };
+        productData();
+    },[])
+
   return (
     <div>
         <table className='table bg-white border border-gray-950 shadow-md ml-6 mt-24'>
@@ -17,15 +33,18 @@ const Home = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td className='text-center border border-gray-950'>sample item</td>
-                    <td className='text-center border border-gray-950'>20 Bags</td>
-                    <td className='text-center border border-gray-950'>2 Bags</td>
-                    <td className='text-center border border-gray-950'>22 Bags</td>
-                    <td className='text-center border border-gray-950'>3 Bags</td>
-                    <td className='text-center border border-gray-950'>19 Bags</td>
-                    <td className='text-center border border-gray-950'><button className='text-xs text-white bg-blue-700 m-1 rounded-md p-1'><FaPen/></button><button className='text-xs text-white bg-red-700 m-1 rounded-md p-1'><FaTrash/></button></td>
-                </tr>
+                {data.map((item) => (
+                     <tr key = {item.id}>
+                     <td className='text-center border border-gray-950'>{item.itemName}</td>
+                     <td className='text-center border border-gray-950'>{item.openingStock}</td>
+                     <td className='text-center border border-gray-950'>{item.newPurchase}</td>
+                     <td className='text-center border border-gray-950'>{item.totalStock}</td>
+                     <td className='text-center border border-gray-950'>{item.output}</td>
+                     <td className='text-center border border-gray-950'>{item.endingStock}</td>
+                     <td className='text-center border border-gray-950'><button className='text-xs text-white bg-blue-700 m-1 rounded-md p-1'><FaPen/></button><button className='text-xs text-white bg-red-700 m-1 rounded-md p-1'><FaTrash/></button></td>
+                 </tr>
+                ))}
+               
             </tbody>
         </table>
     </div>
